d4d57447f719d5757c10515e7989e13483b6a851
Vaishali Verma <vaishalivv@gmail.com>
2024-04-29 10:10:08 -0400

    fixing the footer scroller

--- javascript/core/footer.js
+++ javascript/core/footer.js
@@ -1,10 +1,31 @@
-let observer = new IntersectionObserver(
+let mainfooterObserver = new IntersectionObserver(
+  function (entries) {
+    entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting) {
+        d3.select(".foreground").style("display", "block");
+        d3.select("#callToActionFooterText").attr(
+          "class",
+          "background-text-flex"
+        );
+      } else {
+        d3.select(".foreground").style("display", "none");
+        d3.select("#callToActionFooterText").attr("class", "background-text");
+      }
+    });
+  },
+  { threshold: 0.95 }
+);
+mainfooterObserver.observe(
+  document.querySelector("#callToActionFooterBackground")
+);
+let inFooterObserver = new IntersectionObserver(
   function (entries, observer) {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         if (entry.target.getAttribute("id") == "section0") {
           document
-            .querySelector(".background-text")
+            .querySelector(".background-text-flex")
             .setAttribute("style", "opacity: 0");
           document
             .querySelector(".inner-sticky-text")
@@ -12,7 +33,7 @@ let observer = new IntersectionObserver(
         }
         if (entry.target.getAttribute("id") == "section1") {
           document
-            .querySelector(".background-text")
+            .querySelector(".background-text-flex")
             .setAttribute("style", "opacity: 0.33");
           document
             .querySelector(".inner-sticky-text")
@@ -20,7 +41,7 @@ let observer = new IntersectionObserver(
         }
         if (entry.target.getAttribute("id") == "section2") {
           document
-            .querySelector(".background-text")
+            .querySelector(".background-text-flex")
             .setAttribute("style", "opacity: 0.66");
           document
             .querySelector(".inner-sticky-text")
@@ -28,7 +49,7 @@ let observer = new IntersectionObserver(
         }
         if (entry.target.getAttribute("id") == "section3") {
           document
-            .querySelector(".background-text")
+            .querySelector(".background-text-flex")
             .setAttribute("style", "opacity: 0.99");
           document
             .querySelector(".inner-sticky-text")
@@ -40,7 +61,7 @@ let observer = new IntersectionObserver(
   { root: document.querySelector(".footer"), threshold: 0.5 }
 );
 
-observer.observe(document.querySelector("#section0"));
-observer.observe(document.querySelector("#section1"));
-observer.observe(document.querySelector("#section2"));
-observer.observe(document.querySelector("#section3"));
+inFooterObserver.observe(document.querySelector("#section0"));
+inFooterObserver.observe(document.querySelector("#section1"));
+inFooterObserver.observe(document.querySelector("#section2"));
+inFooterObserver.observe(document.querySelector("#section3"));
